7eeed319ed2c5d83fd13fa9ed33f9cab4d79bea4
dogpatch626 <djpepin1299@gmail.com>
2022-01-30 21:29:43 -0500

    updating

---
+++ src/components/DanceArea.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DanceArea = () => {
+  return (
+    <div className="grid-cols-7 auto-rows-max bg-gray-200 rounded-md my-6 mx-6" style={{height:"100vh"}} >
+    
+      <h1 className="text-3xl font-bold underline">Greetings to all the dancers</h1>
+    </div>
+  );
+};
+
+export default DanceArea;
